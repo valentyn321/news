a2eafd037dc8f8677a3083aff3d654f9382ebe9c
valentyn <valentyncherkasov24@gmail.com>
2020-02-07 11:00:13 +0200

    First commit, base.

---
+++ main/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone
+from ckeditor.fields import RichTextField
+
+class Post(models.Model):
+    title = models.CharField(max_length=256, blank=False)
+    text = RichTextField(blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def publish(self):
+        self.publication_date = timezone.now()
+        self.save()
+
