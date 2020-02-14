f0a68c82ad3318c3402f561a2aff249e68c0d3a9
valentyn <valentyncherkasov24@gmail.com>
2020-02-14 02:14:37 +0200

    Added tests for View, minor fixes.

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from main.models import Post, Comment
+import json
 
-# Create your tests here.
+class TestViews(TestCase):
+
+	def setUp(self):
+		self.client = Client()
+		self.news_list_url = reverse('news_list')
+
+	def test_news_list_GET(self):
+		
+		response = self.client.get(self.news_list_url)
+		self.assertEquals(response.status_code, 200)
+		self.assertTemplateUsed(response, 'main/news_list.html')
+
